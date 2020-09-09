18fd3b23b85e990d7a025fa83a006f55e8527db0
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2020-09-09 16:17:53 -0300

    Bug fix

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV == "production") {
   module.exports = {
-    mongoURI: "mongodb+srv://gabrieladmin:88771797Aa@cluster0.ilmrz.mongodb.net/Cluste0?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://gabrieladmin:88771797Aa@cluster0.ilmrz.mongodb.net/Cluster0?retryWrites=true&w=majority",
   };
 } else {
   module.exports = { mongoURI: "mongodb://admin:password@localhost/blogapp?authSource=admin" };
